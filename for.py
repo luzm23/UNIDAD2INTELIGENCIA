dacaa7d3f51e54d6c7e3ec8036b2a2bf65457fdd
luzm23 <luzmarime7@gmail.com>
2025-02-18 08:59:53 -0600

    for.py

---
+++ for.py
@@ -0,0 +1,8 @@
+
+valores = []
+for i in range(4):
+    valor = input(f"Ingrese el valor {i + 1}: ")
+    valores.append(valor)#agrega
+print("\nValores ingresados:")
+for v in valores:#recorre 
+    print(v)
