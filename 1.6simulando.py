0c4b8851c33f9c96ab6e484c10157ba440a47ede
luzm23 <luzmarime7@gmail.com>
2025-02-25 08:30:37 -0600

    1.6simulando.py

---
+++ 1.6simulando.py
@@ -0,0 +1,12 @@
+opciones = ["ir al cine", "estudiar", "hacer ejercicio"]
+
+
+def tomar_decision(prioridades):
+    for opcion in opciones:
+        if opcion in prioridades:
+            return f"El agente decide: {opcion}"
+        return f"El agente no decide nada"
+    
+
+prioridades = ["hacer ejercicio", "estudiar"]
+print(tomar_decision(prioridades))
